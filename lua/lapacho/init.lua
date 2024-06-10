e3b3b4b6c8f1e65769a17b144c3614bb7a1198ce
rufex <rufex.dev@gmail.com>
2024-06-10 17:41:18 +0200

    Keep track of last active branch
    Keep track of what was the last branch saved
    This is to compare and check if it has changed
    And be able to save or load accordingly

--- lua/lapacho/init.lua
+++ lua/lapacho/init.lua
@@ -15,25 +15,80 @@ local function create_directory(directory_path)
   end)
 end
 
-local function git_branch_shada()
-  local base_dir = vim.fn.stdpath("data") .. "/shadas/"
+local function git_directory_name()
   local git_dir = vim.fn.system("git rev-parse --show-toplevel")
   local git_dir_split = vim.fn.split(git_dir, "/")
-  local git_dir_name = git_dir_split[#git_dir_split]
-  git_dir_name = string.gsub(git_dir_name, "\n", "")
+  local name = git_dir_split[#git_dir_split]
+  name = string.gsub(name, "\n", "")
+  return name
+end
+
+local function git_current_branch_name()
   local branch_name = vim.fn.system("git rev-parse --abbrev-ref HEAD")
-  branch_name = string.gsub(branch_name, "\n", "")
-  create_directory(base_dir)
-  create_directory(base_dir .. git_dir_name)
-  local file_name = base_dir .. git_dir_name .. "/" .. branch_name .. ".shada"
+  return string.gsub(branch_name, "\n", "")
+end
+
+local function lapacho_base_path()
+  local path = vim.fn.stdpath("data") .. "/shadas/"
+  create_directory(path)
+  return path
+end
+
+local function git_directory_path()
+  local path = lapacho_base_path() .. git_directory_name()
+  create_directory(path)
+  return path
+end
+
+local function head_file_path()
+  return git_directory_path() .. "/" .. "head"
+end
+
+local function git_branch_shada_path()
+  local git_dir_path = git_directory_path()
+  local branch_name = git_current_branch_name()
+  local file_name = git_dir_path .. "/" .. branch_name .. ".shada"
   return file_name
+end
+
+local function save_git_head()
+  local branch_name = git_current_branch_name()
+  local head_path = head_file_path()
+  local file = io.open(head_path, "w")
+  if not file then
+    print("Error writing to head file")
+  end
+  file:write(branch_name)
+  file:close()
+end
+
+local function read_git_head()
+  local head_path = head_file_path()
+  local file = io.open(head_path, "r")
+  if not file then
+    save_git_head()
+    file = io.open(head_path, "r")
+  end
+  local branch_name = file:read()
+  file:close()
+  return branch_name
+end
+
+local function head_changed()
+  local current_branch = git_current_branch_name()
+  local head_branch = read_git_head()
+  if current_branch ~= head_branch then
+    save_git_head()
+    return true
+  end
+  return false
 end
 
 -- Events
 local function set_autocmds()
   local group = api.nvim_create_augroup("LapachoEvents", { clear = true })
   -- AutoSave
-  api.nvim_create_autocmd("BufLeave", {
+  api.nvim_create_autocmd({ "BufLeave", "TermLeave" }, {
     pattern = "*",
     group = group,
     callback = function()
@@ -41,7 +96,7 @@ local function set_autocmds()
     end,
   })
   -- AutoLoad
-  api.nvim_create_autocmd("BufEnter", {
+  api.nvim_create_autocmd({ "BufEnter", "TermEnter" }, {
     pattern = "*",
     group = group,
     callback = function()
@@ -62,6 +117,7 @@ local M = {}
 
 -- Initialize
 -- @param opts? table
+-- @param opts.disable_autocmds? boolean
 M.setup = function(opts)
   opts = opts or {}
 
@@ -77,15 +133,22 @@ M.PrintDir = function()
   print(base_dir)
 end
 
+M.PrintDir = function()
+  print(lapacho_base_path())
+end
+
 M.LapachoSave = function()
-  local filename = git_branch_shada()
+  if head_changed() then
+    return
+  end
+  local filename = git_branch_shada_path()
   vim.cmd("wshada " .. filename)
   print("Branch shada file saved to " .. filename)
 end
 
 M.LapachoLoad = function()
   vim.cmd("delmark A-Z")
-  local filename = git_branch_shada()
+  local filename = git_branch_shada_path()
   local stat = uv.fs_stat(filename)
   if not stat then
     print("Branch shada file not found: " .. filename)
